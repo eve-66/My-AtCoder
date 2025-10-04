5e337531a9d6bebc8c294bc3d15df8061dc8ac54
eve-66 <is0733pi@ed.ritsumei.ac.jp>
2025-10-04 23:11:03 +0900

    10/4

--- C-426.py
+++ C-426.py
@@ -1,12 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 N, Q = map(int, input().split())
-min_ver = 0
-versions = [i for i in range(1, int(N)+1)]
+pc = [0] * (N + 2)  # バージョンごとの台数
+for i in range(1, N + 1):
+    pc[i] = 1  # 初期状態は1台ずつ
+
+O = 1  # 最古のバージョン
 
 for _ in range(Q):
     X, Y = map(int, input().split())
-    if X <= min_ver:
-        print(0)
-        continue
-    print(versions[X-1])
-    versions[Y-1] += versions[X-1]
-    min_ver = X
+    cnt = 0
+    while O <= X:
+        cnt += pc[O]        # アップグレードされる台数
+        pc[Y] += pc[O]      # 新しいバージョンに追加
+        pc[O] = 0           # 古いバージョンは0に
+        O += 1
+    print(cnt)
